4f5c5f74a680e1efa41f5234981f17983b409ca0
Grian Gajila <griangajila2@gmail.com>
2025-05-26 18:03:42 +0800

    wip

---
+++ components/index.ts
@@ -0,0 +1,17 @@
+import CustomButton from "./custom-button";
+import DrawerIcon from "./drawer-icon";
+import Header from "./header";
+import InputField from "./input-fields";
+import Logout from "./logout";
+import ProfileInfo from "./profile-info";
+import Settings from "./settings";
+
+export {
+	CustomButton,
+	DrawerIcon,
+	Header,
+	InputField,
+	Logout,
+	ProfileInfo,
+	Settings,
+};

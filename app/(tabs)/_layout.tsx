e4405fe050f4763011a6d8d0f3f2b6b9946c8d0e
Grian Gajila <griangajila2@gmail.com>
2025-05-26 18:26:06 +0800

    wip

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,65 @@
+import { Header } from "@/components";
+import { useAuth } from "@/context/auth-context";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import { Redirect, Tabs, usePathname } from "expo-router";
+import React from "react";
+
+export default function TabLayout() {
+	const { authState } = useAuth();
+	const [isAuthenticated, setIsAthenticated] = React.useState(false);
+	const pathName = usePathname();
+
+	React.useEffect(() => {
+		if (!authState?.authenticated) {
+			setIsAthenticated(true);
+			return;
+		}
+		setIsAthenticated(false);
+		return;
+	}, [authState]);
+
+	if (isAuthenticated) {
+		return <Redirect href={"/sign-in"} />;
+	}
+
+	return (
+		<>
+			<Tabs
+				screenOptions={{
+					headerShadowVisible: false,
+					tabBarActiveTintColor: "#00ffb2",
+					tabBarInactiveTintColor: "white",
+					tabBarStyle: {
+						backgroundColor: "#0a0f1c",
+						borderTopWidth: 0,
+					},
+				}}
+			>
+				<Tabs.Screen
+					name="Home"
+					options={{
+						href: pathName === "/Control" ? null : undefined,
+						title: "Home",
+						headerRight: () => {
+							return pathName === "/Control" ? "" : <Header.HeaderRight />;
+						},
+						headerStyle: {
+							backgroundColor: "#0a0f1c",
+						},
+						headerTintColor: "white",
+
+						tabBarIcon: ({ color, focused }) =>
+							pathName !== "/Control" && (
+								<FontAwesome6
+									size={24}
+									name="house"
+									color={color}
+									focusable={focused}
+								/>
+							),
+					}}
+				/>
+			</Tabs>
+		</>
+	);
+}

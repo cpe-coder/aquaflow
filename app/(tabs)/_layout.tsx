e00cb21ef92956dfaa9e7f9afcce9d68b54871d5
Grian Gajila <griangajila2@gmail.com>
2025-05-29 21:05:47 +0800

    wip

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -21,7 +21,29 @@ export default function TabLayout() {
 
 	return (
 		<>
-			<Tabs></Tabs>
+			<Tabs>
+				<Tabs.Screen
+					name="Home"
+					options={{
+						title: "Home",
+						headerShown: false,
+					}}
+				/>
+				<Tabs.Screen
+					name="Profile"
+					options={{
+						title: "Profile",
+						headerShown: false,
+					}}
+				/>
+				<Tabs.Screen
+					name="Settings"
+					options={{
+						title: "Settings",
+						headerShown: false,
+					}}
+				/>
+			</Tabs>
 		</>
 	);
 }

28a7f393f8e245fab7d480fb2f2bae4fdc809019
Grian Gajila <griangajila2@gmail.com>
2025-05-30 20:04:28 +0800

    wip

--- app/(tabs)/Control.tsx
+++ app/(tabs)/Control.tsx
@@ -1,10 +1,12 @@
-import { useNavigation } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useNavigation, useRouter } from "expo-router";
 import * as ScreenOrientation from "expo-screen-orientation";
 import React from "react";
-import { Text, View } from "react-native";
+import { Pressable, Text, TouchableOpacity, View } from "react-native";
 
 export default function Control() {
 	const navigation = useNavigation();
+	const router = useRouter();
 	React.useEffect(() => {
 		const unsubscribe = navigation.addListener("focus", () => {
 			try {
@@ -18,9 +20,27 @@ export default function Control() {
 
 		return unsubscribe;
 	}, [navigation]);
+
+	const handleBackPress = () => {
+		router.back();
+	};
+
 	return (
-		<View className="bg-background h-full w-full px-8">
-			<Text>Control</Text>
+		<View className="bg-background h-full w-full px-8 flex-row">
+			<View className="h-full w-52">
+				<Pressable onPress={handleBackPress} className=" mr-5 bg-black">
+					<Ionicons name="arrow-back" size={24} color="white" />
+				</Pressable>
+			</View>
+			<View className="flex-1 h-full bg-background2">
+				<TouchableOpacity
+					onPress={() => console.log("Clicked")}
+					className="bg-slate-500 rounded-full p-4 w-10 h-10"
+				>
+					<Text className="text-white">Hello</Text>
+				</TouchableOpacity>
+			</View>
+			<View className="h-full w-52"></View>
 		</View>
 	);
 }
